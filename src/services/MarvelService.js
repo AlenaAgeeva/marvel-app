ce9b329b95ca31a6569b38e6aebe7ca400679950
Alena Ageeva <alenaageevapv@gmail.com>
2023-07-10 21:27:17 -0300

    Add a service component for sending GET requests to the Marvel server. Create 2 function for getting a single charachter and all characters information

---
+++ src/services/MarvelService.js
@@ -0,0 +1,33 @@
+class MarvelService {
+  _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  _apiKey = "apikey=b125aeeb9ecbb13ceae75d89d3b84699";
+  getResource = async (url) => {
+    let res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+  };
+  getAllCharacters = async () => {
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharacter);
+  };
+  getSingleCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
+  };
+  _transformCharacter = (character) => {
+    return {
+      name: character.name,
+      description: character.description,
+      thumbnail: character.thumbnail.path + "." + character.thumbnail.extension,
+      homepage: character.urls[0].url,
+      wiki: character.urls[1].url,
+    };
+  };
+}
+export default MarvelService;
